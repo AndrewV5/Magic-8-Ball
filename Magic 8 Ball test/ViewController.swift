b9f20efc0ef24c7ec2b748a1698732bd21ed5711
Your Name <you@example.com>
2018-09-10 14:42:40 -0600

    Completed app

--- Magic 8 Ball test/ViewController.swift
+++ Magic 8 Ball test/ViewController.swift
@@ -9,17 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var randomBallIndex : Int = 0
+    let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
 
+    @IBOutlet weak var ballImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateBallImage()
     }
+
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func answerButtonClicked(_ sender: UIButton) {
+        updateBallImage()
+    }
+    func updateBallImage(){
+        randomBallIndex = Int(arc4random_uniform(5))
+        ballImageView.image = UIImage(named: ballArray[randomBallIndex])
+        
+    }
 
 }
 
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Magic 8 Ball App
+
+Name: Andrew Vergara
+
+
+UserID: AndrewV5
+
+
+Date Completed: 09/10/18
+# What the app looks like
+![alt text](https://raw.githubusercontent.com/AndrewV5/Magic-8-Ball/blob/master/Screen%20Shot%202018-09-10%20at%202.40.10%20PM.png)
